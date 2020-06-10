daa5a8f93e6cd1c5dc33106523e0c0e761791d8f
ROHIT PASUMARTHI <53930875+ROHIT-994@users.noreply.github.com>
2020-06-10 08:17:40 +0530

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.Scanner;
+class Three
+{
+   public static void main(String[] args)
+   {
+      int b=Integer.parseInt(args[0]);
+     int c=Integer.parseInt(args[1]);
+     System.out.println(b+c);
+   }
+}
